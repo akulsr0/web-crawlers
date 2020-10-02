4b1d8c185b3026cfe4a79e747a271f36816c3353
Abhijeet Srivastav <56226285+AbhijeetSrivastav@users.noreply.github.com>
2020-10-02 11:24:10 +0530

    Update uifaces.py
    Well commented the code and improved the syntax.

--- uifaces.py
+++ uifaces.py
@@ -1,9 +1,12 @@
+#Importing the libraries
 from bs4 import *
 import requests as rq
 import os
 
-print("Choose Gender: 1) Male  2) Female  3) Both")
-genderInput = int(input())
+#Taking the gender input
+genderInput = int(input("Choose Gender: 1) Male  2) Female  3) Both"))
+
+#Gender case switching
 if genderInput==1:
     r2 = rq.get('https://uifaces.co/?from_age=18&to_age=40&gender%5B%5D=male')
 elif genderInput==2:
@@ -11,15 +14,19 @@ elif genderInput==2:
 else:
     r2 = rq.get('https://uifaces.co/')
 
+#Selecting the parser and creating its instance
 soup2 = BeautifulSoup(r2.text, "html.parser")
 links = []
 
+#Providing the urls
 x = soup2.select('img[data-original^="https://images.pexels.com/photos/"]')
 y = soup2.select('img[data-original^="https://images.unsplash.com/"]')
 z = soup2.select('img[data-original^="https://randomuser.me/api/portraits/"]')
 a = soup2.select('img[data-original^="https://tinyfac.es/data/avatars/"]')
 b = soup2.select('img[data-original^="https://images-na.ssl-images-amazon.com/images/"]')
 
+
+#Appending the list with prefered output
 for img in x:
     links.append(img['data-original'])
 
@@ -36,13 +43,17 @@ for img in b:
     links.append(img['data-original'])
 
 
+#Setting download path
 path = "Downloads/"
-
 os.mkdir(str(path))
+
+#Asking for image count input
 imagesCount = int(input("How many images do you want: "))
 if imagesCount>len(links):
     imagesCount=len(links)
 i=1
+
+#Enumerating the link
 for index, img_link in enumerate(links):
     if i<=imagesCount:
         img_data = rq.get(img_link).content
